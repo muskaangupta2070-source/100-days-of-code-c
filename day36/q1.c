781433f99a84601de2eba64fe69984577b5ab8de
muskaangupta2070-source <muskaangupta2070@gmail.com>
2026-09-14 16:12:51 +0530

    day 36 q1 completed

---
+++ day36/q1.c
@@ -0,0 +1,38 @@
+//Q71: Read and print a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+Output 1:
+1 2
+3 4
+
+*/
+#include <stdio.h>
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+    int matrix[rows][cols];
+    printf("\nEnter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("\nThe entered matrix is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d\t", matrix[i][j]); 
+        }
+        printf("\n"); 
+    }
+    return 0;
+}
